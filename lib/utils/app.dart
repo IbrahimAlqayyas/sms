94a08edce73bf56eefb36a4728efa0bb48ec75e2
Ibrahim Alqayyas <ibrahim.alqayyas@gmail.com>
2023-02-04 21:51:19 +0200

    :pushpin: new release

--- lib/utils/app.dart
+++ lib/utils/app.dart
@@ -2,4 +2,4 @@ import 'package:flutter/foundation.dart';
 
 const bool release = kReleaseMode;
 const bool debug = kDebugMode;
-const String kVersion = 'v1.0.0';
+const String kVersion = 'v1.0.0+2';
